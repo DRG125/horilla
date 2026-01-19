ac320a9f9a4677cb0fd16a861a20d1e7bc8bd7d8
DRG125 <dadrgunawardhana@gmail.com>
2026-01-19 23:38:28 +0530

    Initial commit with start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ python manage.py migrate
 echo "Collecting static files…"
 python manage.py collectstatic --noinput
 
-echo "Starting server without schedulers…"
-gunicorn horilla.wsgi:application
+echo "Starting server on port $PORT…"
+gunicorn horilla.wsgi:application --bind 0.0.0.0:$PORT
