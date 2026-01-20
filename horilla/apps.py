d86c83b2e73c8977ca417beec44cabc142a24b54
DRG125 <dadrgunawardhana@gmail.com>
2026-01-20 14:16:45 +0530

    Include superuser details updated

--- horilla/apps.py
+++ horilla/apps.py
@@ -10,13 +10,16 @@ class HorillaConfig(AppConfig):
         User = get_user_model()
         admin_email = os.getenv("ADMIN_EMAIL")
         admin_password = os.getenv("ADMIN_PASSWORD")
+        admin_username = os.getenv("ADMIN_USERNAME")  # Add this env var
 
-        if not admin_email or not admin_password:
+        if not admin_email or not admin_password or not admin_username:
             return
 
         if not User.objects.filter(is_superuser=True).exists():
             User.objects.create_superuser(
+                username=admin_username,
                 email=admin_email,
                 password=admin_password,
             )
             print("✅ Superuser created automatically")
+
