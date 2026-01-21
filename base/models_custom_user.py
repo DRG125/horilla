a0dba5ae7eb811ef0ea775fd727bae4579321dda
DRG125 <dadrgunawardhana@gmail.com>
2026-01-21 18:19:18 +0530

    Include is new employee issues updated

--- base/models_custom_user.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-class CustomUser(AbstractUser):
-    is_new_employee = models.BooleanField(default=False, verbose_name=_("Is New Employee"))
