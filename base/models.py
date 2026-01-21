91da8af10414605692004470c868038f499123b1
DRG125 <dadrgunawardhana@gmail.com>
2026-01-21 21:16:30 +0530

    Include superuser creation issues updated

--- base/models.py
+++ base/models.py
@@ -1864,4 +1864,4 @@ class NotificationSound(models.Model):
     sound_enabled = models.BooleanField(default=False)
 
 
-User.add_to_class("is_new_employee", models.BooleanField(default=False))
+#User.add_to_class("is_new_employee", models.BooleanField(default=False))
